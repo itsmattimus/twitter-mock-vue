2a1f1d92709d15296390ae5cea39378723e4dcf2
itsmattimus <matt.bennett1795@gmail.com>
2020-07-02 21:06:51 -0400

    updated project to reflect vue cli update

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import { auth } from './firebase'
+import './assets/scss/app.scss'
+
+Vue.config.productionTip = false
+
+let app
+auth.onAuthStateChanged(user => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+  //dispatch an action on a page reload if a user is logged in
+  if (user) {
+    store.dispatch('fetchUserProfile', user)
+  }
+})
+
+
